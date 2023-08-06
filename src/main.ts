4836b3955a34b0ffc54906839d728f94cb534f42
tangytin@163.com <tangytin@163.com>
2023-08-06 12:49:30 +0800

    fix：处理vant和在h5端和微信小程序端的兼容性问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { createSSRApp } from 'vue'
+import { createSSRApp, useAttrs } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersist from 'pinia-plugin-persist'
 import App from './App.vue'
+// 微信平台时，不引入vant
+// #ifndef MP-WEIXIN
+import vant from 'vant'
+import 'vant/lib/index.css'
+// #endif
 export function createApp() {
   const app = createSSRApp(App)
+  const pinia = createPinia()
+  pinia.use(piniaPluginPersist)
+  app.use(pinia)
+  // #ifndef MP-WEIXIN
+  app.use(vant)
+  // #endif
   return {
     app,
   }
