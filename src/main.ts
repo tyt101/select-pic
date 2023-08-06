7b47500cea919caa270c0f4610cb0b9e9c3bfb34
tangytin@163.com <tangytin@163.com>
2023-08-06 22:18:23 +0800

    去掉组件库

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { createSSRApp, useAttrs } from 'vue'
 import App from './App.vue'
 import store from './store'
-// 微信平台时，不引入vant
-// #ifndef MP-WEIXIN
-import vant from 'vant'
-import 'vant/lib/index.css'
-// #endif
 export function createApp() {
   const app = createSSRApp(App)
   app.use(store)
-  // #ifndef MP-WEIXIN
-  app.use(vant)
-  // #endif
   return {
     app,
   }
