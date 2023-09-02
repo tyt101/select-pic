cb5e8cec5e340455aaf557e5f2d431a01576ec11
tangytin@163.com <tangytin@163.com>
2023-09-03 00:06:15 +0800

    feat: 底部栏标识公共组件

---
+++ src/components/bottom.vue
@@ -0,0 +1,12 @@
+<template>
+  <view class="bottom">@一秋选片技术支持</view>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.bottom {
+  padding: 30rpx 0 50rpx;
+  font-size: 25rpx;
+  text-align: center;
+  color: #808080;
+}
+</style>
---
+++ src/components/invite.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="container">
+    <view class="circle">
+      <span>15</span>
+      <span class="invite">邀</span>
+    </view>
+    <view class="text">邀请用户可以升级功能或延长服务</view>
+  </view>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.circle {
+  position: relative;
+  border-radius: 50%;
+  width: 60rpx;
+  height: 60rpx;
+  text-align: center;
+  line-height: 60rpx;
+  border: 1rpx solid #808080;
+}
+.invite {
+  position: absolute;
+  top: -30rpx;
+  right: 10rpx;
+}
+.text {
+  width: 250rpx;
+  align-self: center;
+  margin-top: 25rpx;
+  color: #808080;
+}
+</style>
